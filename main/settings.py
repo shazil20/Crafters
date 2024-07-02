b25ac083fac7920a9cc18020b4f567bc116226cc
Muhammad Shazil <shazil03144426622@gmail.com>
2024-07-02 16:34:18 +0500

    settings.py

--- main/settings.py
+++ main/settings.py
@@ -191,9 +191,3 @@ SIMPLE_JWT = {
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'minahil510malik@gmail.com'  # Replace with your Gmail email address
-EMAIL_HOST_PASSWORD = 'vwny cghq sqpi qxza'
